a3c95963442eb4843f8a37df1fb458e7f5fed245
Muhammad Imam Choirudin <mimamch28@gmail.com>
2022-01-04 14:49:25 +0700

    adding slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,54 @@ bannerBtn.forEach((e, i) => {
     counter = i + 1;
   });
 });
+
+let teamMargin = 0;
+let teamView = 0;
+
+const prevBtn = document.querySelector(".btn.prev");
+const nextBtn = document.querySelector(".btn.next");
+const teamContent = document.querySelectorAll(".team-content");
+
+teamContent.forEach(() => {
+  const teamStrip = document.createElement("LI");
+  const teamNavigation = document.querySelector(".team-navigation");
+  teamNavigation.insertBefore(teamStrip, nextBtn);
+});
+prevBtn.addEventListener("click", () => {
+  teamPrev();
+});
+nextBtn.addEventListener("click", () => {
+  teamNext();
+});
+
+const teamNext = () => {
+  if (teamMargin !== -(teamContent.length * 100 - 100)) {
+    teamMargin -= 100;
+  } else {
+    teamMargin = 0;
+  }
+  teamContent[0].style.marginLeft = `${teamMargin}%`;
+  coloringStrip(-teamMargin / 100);
+};
+
+const teamPrev = () => {
+  if (teamMargin < 0) {
+    teamMargin += 100;
+  }
+  teamContent[0].style.marginLeft = `${teamMargin}%`;
+  coloringStrip(-teamMargin / 100);
+};
+
+setInterval(() => {
+  teamNext();
+  coloringStrip(-teamMargin / 100);
+}, 20000);
+
+const teamStrip = document.querySelectorAll(".team-navigation li");
+const coloringStrip = (i) => {
+  teamStrip.forEach((e) => (e.style.backgroundColor = "rgba(0, 0, 0, 0.123)"));
+
+  teamStrip[i].style.backgroundColor = "#ffb000";
+};
+
+coloringStrip(0);
